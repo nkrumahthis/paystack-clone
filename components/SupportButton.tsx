37c0cb5d3ad6df49858b99529ab9bc726722d150
Nkrumah <nkrumahthis@gmail.com>
2023-01-08 12:53:41 +0000

    support button

---
+++ components/SupportButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SupportButton() {
+    return (
+        <button className="absolute bottom-0 right-0 bg-gray-900 text-gray-300 hover:text-white p-3 rounded-full text-sm m-4">
+            ? SUPPORT
+        </button>
+    )
+}
+
+export default SupportButton
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Content from '../components/Content'
+import SupportButton from '../components/SupportButton'
 
 function dashboard() {
     return (
         <div className="flex h-screen">
             <Sidebar />
             <Content />
+            <SupportButton />
         </div >
     )
 }
