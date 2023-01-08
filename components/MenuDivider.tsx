f5948a3ba98c43a98c30f20db2f51f5396f9164a
Nkrumah <nkrumahthis@gmail.com>
2023-01-08 14:29:24 +0000

    style menu divider

--- components/MenuDivider.tsx
+++ components/MenuDivider.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function MenuDivider() {
+function MenuDivider(props: { label: string }) {
+    let { label } = props
     return (
-        <div>MenuDivider</div>
+        <div className='text-slate-400 text-xs mt-5 mb-1 ml-6'>{label}</div>
     )
 }
 
--- components/SidebarContent.tsx
+++ components/SidebarContent.tsx
@@ -6,7 +6,8 @@ function SidebarContent() {
     return (
         <div className="flex flex-col grow my-2 overflow-y-scroll h-full">
             <MenuLink chosen={false} label="Compliance" logo="⟥" />
-            <MenuLink chosen={false} label="Home" logo="⌂" />
+            <MenuLink chosen={true} label="Home" logo="⌂" />
+            <MenuDivider label="PAYMENTS" />
             <MenuLink chosen={false} label="Transactions" logo="⌂" />
             <MenuLink chosen={false} label="Customers" logo="⌂" />
             <MenuLink chosen={false} label="Refunds" logo="⌂" />
@@ -15,12 +16,15 @@ function SidebarContent() {
             <MenuLink chosen={false} label="Transaction Splits" logo="⌂" />
             <MenuLink chosen={false} label="Subaccounts" logo="⌂" />
             <MenuLink chosen={false} label="Terminals" logo="⌂" />
+            <MenuDivider label="RECURRING" />
             <MenuLink chosen={false} label="Subscribers" logo="⌂" />
             <MenuLink chosen={false} label="Plans" logo="⌂" />
             <MenuLink chosen={false} label="Subscriptions" logo="⌂" />
+            <MenuDivider label="TRANSFERS" />
             <MenuLink chosen={false} label="Transfers" logo="⌂" />
             <MenuLink chosen={false} label="Recipients" logo="⌂" />
             <MenuLink chosen={false} label="Balance" logo="⌂" />
+            <MenuDivider label="COMMERCE" />
             <MenuLink chosen={false} label="Payment Pages" logo="⌂" />
             <MenuLink chosen={false} label="Products" logo="⌂" />
             <MenuLink chosen={false} label="Storefronts" logo="⌂" />
