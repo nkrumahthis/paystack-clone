de4cfc409e1c479b4e515fe240962e49df16b292
Nkrumah <nkrumahthis@gmail.com>
2023-01-08 13:24:36 +0000

    styled sidebar user

--- components/SidebarUser.tsx
+++ components/SidebarUser.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function SidebarUser() {
     return (
-        <div className='m-2 bg-gray-700 p-3'><h3>Dext Technology</h3><p className='text-slate-400 text-xs'>410597</p></div>
+        // <div className='m-2 bg-gray-700 py-3 px-12'><h3>Dext Technology</h3><p className='text-slate-400 text-xs'>410597</p></div>
+        <div className='m-2 bg-gray-700 py-2 px-4'>
+            <p className='text-md text-ellipsis text-capitalize'>Dext Technology</p>
+            <p className='text-ellipsis text-capitalize font-bold' style={{ color: "#91A0AF" }}>410597</p>
+        </div>
 
     )
 }
