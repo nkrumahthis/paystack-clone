b9b96b8aa85ff877d1fa6e1a55c0b6a14fad504e
Nkrumah <nkrumahthis@gmail.com>
2023-01-08 14:29:43 +0000

    style chosen menuLink

--- components/MenuLink.tsx
+++ components/MenuLink.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 function MenuLink(props: { chosen: boolean, label: string, logo: string }) {
     let { chosen, label, logo } = props
+    let chosenStyle = "text-blue-600"
     return (
-        <Link href="#" className='py-1 pl-6 text-sm hover:bg-gray-800 w-full'><i>{logo}</i> {label}</Link>
+        <Link href="#" className={'py-1 pl-6 text-sm hover:bg-gray-800 w-full my-1 ' + (chosen ? chosenStyle : '')}><i>{logo}</i> {label}</Link>
     )
 }
 
